bc70185775c22080d5e0a192299868598f5b1ee3
ecitak <85674119+ecitak@users.noreply.github.com>
2023-04-12 14:18:50 +0300

    An interface was added.

---
+++ Cassandra.Core/ICassandraRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cassandra.Core
+{
+    public interface ICassandraRepository
+    {
+        /// <summary>
+        /// It asynchronously returns the model based on the ID value
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<TEntity> GetByIdAsync(string id);
+
+        /// <summary>
+        /// It asynchronously returns all records as IEnumerable<TEntity>
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        /// <summary>
+        /// It asynchronously performs the operation of adding a new record.
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task AddAsync(TEntity entity);
+
+        /// <summary>
+        /// It asynchronously performs the update operation.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task UpdateAsync(string id, TEntity entity);
+
+        /// <summary>
+        /// It asynchronously performs the delete operation.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task DeleteAsync(string id);
+    }
+}
