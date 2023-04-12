366c200bb83ad4ce4b77bc08fa16ea331e57f0eb
ecitak <85674119+ecitak@users.noreply.github.com>
2023-04-12 14:25:40 +0300

    The ICassandraRepository was implemented in CassandraRepository. The constructor for CassandraRepository was defined.

--- Cassandra.DataAccessLayer/CassandraRepository.cs
+++ Cassandra.DataAccessLayer/CassandraRepository.cs
@@ -2,8 +2,51 @@
 
 namespace Cassandra.DataAccessLayer
 {
-    public class CassandraRepository<TEntity> 
+    public class CassandraRepository<TEntity> : ICassandraRepository<TEntity>
     {
-  
+        private readonly ISession _session;
+        private readonly Cluster _cluster;
+        private string _keyspace;
+
+        public CassandraRepository
+        (
+            string keyspace
+            , string contactPoint = "localhost"
+        )
+        {
+            _keyspace = keyspace;
+            _cluster = Cluster
+                          .Builder()
+                          .AddContactPoint(contactPoint)
+                          .Build();
+
+            _session = _cluster
+                       .Connect(keyspace);
+        }
+
+        public Task AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(string id, TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
